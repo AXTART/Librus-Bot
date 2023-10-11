ba9daa700cb1d10c7a4430ebcfefa9c18aac8078
AXTART <80387224+AXTART@users.noreply.github.com>
2023-10-11 23:02:06 +0200

    v.1.0.6

--- commands/librus/szczensliwy-numerek.js
+++ commands/librus/szczensliwy-numerek.js
@@ -2,17 +2,16 @@ const { SlashCommandBuilder } = require('discord.js');
 const { LibrusLogin, LibrusPassword } = require('../../config.json');
 const Librus = require("librus-api");
 let lclient = new Librus();
-
-module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('szczęśliwy-numerek')
-		.setDescription('Wyświetl szczęśliwy numerek'),
-	async execute(interaction) {
-        console.log('Użyto funkcji "/szczęśliwy-numerek"')
-         lclient.authorize(LibrusLogin,LibrusPassword).then(function () {
+lclient.authorize(LibrusLogin,LibrusPassword).then(function () {
+    module.exports = {
+	    data: new SlashCommandBuilder()
+		    .setName('szczęśliwy-numerek')
+		    .setDescription('Wyświetl szczęśliwy numerek'),
+	    async execute(interaction) {
+            console.log('Użyto funkcji "/szczęśliwy-numerek"')
             lclient.info.getLuckyNumber().then((lucky) => {
-            interaction.reply("**" + lucky.toString() + "**")
+                interaction.reply("**" + lucky.toString() + "**")
             });
-        });
-    },
-};
+        },
+    };
+});
