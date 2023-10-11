3673d2d09b3d99dd80b9a0829e0333898543c400
AXTART <80387224+AXTART@users.noreply.github.com>
2023-10-11 23:26:05 +0200

    v1.0.8

--- commands/librus/ogloszenia.js
+++ commands/librus/ogloszenia.js
@@ -3,28 +3,25 @@ const { LibrusLogin, LibrusPassword } = require('../../config.json');
 const Librus = require("librus-api");
 let lclient = new Librus();
 
-lclient.authorize(LibrusLogin, LibrusPassword).then(function () {
-	module.exports = {
-	
-		data: new SlashCommandBuilder()
-			.setName('ogłoszenia')
-			.setDescription('Wyświetl ogłoszenia'),
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('ogłoszenia')
+		.setDescription('Wyświetl ogłoszenia'),
     
-		async execute(interaction) {
-        		lclient.inbox.listAnnouncements().then(data => {
-        			const embed = new EmbedBuilder()
-	        			.setColor(0x0099FF)
-	        			.setTitle(data[0].title)
-	        			.setAuthor({ name: data[0].user, iconURL: 'https://tinyurl.com/yc5pch7k' })
-	        			.setDescription(data[0].content)
-	        			.setTimestamp()
-	        			.setFooter({ text: 'LibrusBot', iconURL: 'https://tinyurl.com/yc5pch7k' });
+	async execute(interaction) {
+		lclient.authorize(LibrusLogin, LibrusPassword).then(function () {
+        	lclient.inbox.listAnnouncements().then(data => {
+        		const embed = new EmbedBuilder()
+	        		.setColor(0x0099FF)
+	        		.setTitle(data[0].title)
+	        		.setAuthor({ name: data[0].user, iconURL: 'https://tinyurl.com/yc5pch7k' })
+	        		.setDescription(data[0].content)
+	        		.setTimestamp()
+	        		.setFooter({ text: 'LibrusBot', iconURL: 'https://tinyurl.com/yc5pch7k' });
 				console.log('Użyto funkcji "/Ogłoszenia"')
-        			interaction.reply({embeds: [embed],});
-     			});
-		},
-	};
-});
-
-
-
+        		interaction.reply({embeds: [embed],});
+     		});
+		});
+	}, 
+};
